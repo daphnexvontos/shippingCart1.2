d0b18c9d4d6e8f9cd4bf5f5619db3ddda7ca21d9
daphnexvontos <138835985+daphnexvontos@users.noreply.github.com>
2023-11-18 17:28:27 +0800

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/shipping_cart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vontos.shippingcart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/vontos/shipping_cart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vontos.shipping_cart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
